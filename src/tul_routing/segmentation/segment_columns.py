7ebdd02c0f4b984b815ac6d6a52e1e86f906e326
Jan Hybs <hybs.j@st-software.com>
2023-10-22 15:55:14 +0200

    feat: update body, add strategy processing template

--- src/tul_routing/segmentation/segment_columns.py
+++ src/tul_routing/segmentation/segment_columns.py
@@ -1,11 +1,19 @@
 import pandas as pd
+import numpy as np
 from ..config import Config
 
 
 def segment_columns(df: pd.DataFrame, options: Config) -> pd.DataFrame:
-    print("resample_features...")
-
+    remaining_columns = list(df.columns)
     for k, v in options.segmentation_options.items():
-        print(f"resample_features: {k} -> {v}")
+        print(f"segment_columns: {k} -> {v}")
+        remaining_columns.remove(k)
+
+    for name, dtype in df.dtypes.items():
+        if name in remaining_columns:
+            if dtype == np.float64:
+                ... # linear interpolation
+            else:
+                raise Exception(f"Column {name} is of type {dtype} does not have a segmentation strategy")
 
     return df
